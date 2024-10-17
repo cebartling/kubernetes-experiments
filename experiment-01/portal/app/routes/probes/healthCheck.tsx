5d86e4832c4fef88ac41ca2ff95ee3df875a324c
Christopher Bartling <chris.bartling@gmail.com>
2024-10-16 20:19:51 -0500

    feat: adding health and readiness checks

---
+++ experiment-01/portal/app/routes/probes/healthCheck.tsx
@@ -0,0 +1,12 @@
+import { LoaderFunction } from '@remix-run/node';
+
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return new Response('OK', {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
