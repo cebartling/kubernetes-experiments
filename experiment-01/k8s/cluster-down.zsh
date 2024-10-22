948da56015c7e1b3d4582c04453c3d66224dd951
Christopher Bartling <chris.bartling@gmail.com>
2024-10-21 22:25:10 -0500

    feat: adding helm charts

--- experiment-01/k8s/cluster-down.zsh
+++ experiment-01/k8s/cluster-down.zsh
@@ -4,32 +4,33 @@
 # It is used to delete the namespace, pods and services
 
 # Delete the pods and services
-kubectl delete -f ./k8s/templates/api-server/kubernetes-experiments-01-api-server-deployment.yaml
-kubectl delete -f ./k8s/templates/api-server/kubernetes-experiments-01-api-server-service.yaml
+#kubectl delete -f ./k8s/templates/api-server/kubernetes-experiments-01-api-server-deployment.yaml
+#kubectl delete -f ./k8s/templates/api-server/kubernetes-experiments-01-api-server-service.yaml
 
-kubectl delete -f ./k8s/templates/portal/kubernetes-experiments-01-portal-deployment.yaml
-kubectl delete -f ./k8s/templates/portal/kubernetes-experiments-01-portal-service.yaml
-kubectl delete -f ./k8s/templates/portal/kubernetes-experiments-01-portal-ingress.yaml
+#kubectl delete -f ./k8s/templates/portal/kubernetes-experiments-01-portal-deployment.yaml
+#kubectl delete -f ./k8s/templates/portal/kubernetes-experiments-01-portal-service.yaml
+#kubectl delete -f ./k8s/templates/portal/kubernetes-experiments-01-portal-ingress.yaml
 
-kubectl delete -f ./k8s/templates/admin-portal/kubernetes-experiments-01-admin-portal-deployment.yaml
-kubectl delete -f ./k8s/templates/admin-portal/kubernetes-experiments-01-admin-portal-service.yaml
-kubectl delete -f ./k8s/templates/admin-portal/kubernetes-experiments-01-admin-portal-ingress.yaml
+cd ./admin-portal || exit
+helm uninstall kubernetes-experiments-01-admin-portal-0.0.1.tgz --namespace=kubernetes-experiments-01
+cd ..
+
 
 # Remove Redis
-kubectl delete -f ./k8s/templates/redis/redis-service.yaml
-kubectl delete -f ./k8s/templates/redis/redis-statefulset.yaml
-kubectl delete -f ./k8s/templates/redis/redis-configmap.yaml
+#kubectl delete -f ./k8s/templates/redis/redis-service.yaml
+#kubectl delete -f ./k8s/templates/redis/redis-statefulset.yaml
+#kubectl delete -f ./k8s/templates/redis/redis-configmap.yaml
 
 # Remove PostgreSQL
-kubectl delete -f ./k8s/templates/postgresql/postgresql-service.yaml
-kubectl delete -f ./k8s/templates/postgresql/postgresql-statefulset.yaml
-kubectl delete -f ./k8s/templates/postgresql/postgresql-secret.yaml
-kubectl delete -f ./k8s/templates/postgresql/postgresql-configmap.yaml
+#kubectl delete -f ./k8s/templates/postgresql/postgresql-service.yaml
+#kubectl delete -f ./k8s/templates/postgresql/postgresql-statefulset.yaml
+#kubectl delete -f ./k8s/templates/postgresql/postgresql-secret.yaml
+#kubectl delete -f ./k8s/templates/postgresql/postgresql-configmap.yaml
 
 kubectl get pods,deployments,services,statefulsets --namespace=kubernetes-experiments-01
 
 # delete the namespace
-kubectl delete -f ./k8s/templates/kubernetes-experiments-01-namespace.yaml
+kubectl delete -f ./templates/kubernetes-experiments-01-namespace.yaml
 
 kubectl get namespaces
 
--- experiment-01/k8s/cluster-up.zsh
+++ experiment-01/k8s/cluster-up.zsh
@@ -4,29 +4,34 @@
 # It is used to create the namespace, pods and services
 
 # Apply the namespace
-kubectl apply -f ./k8s/templates/kubernetes-experiments-01-namespace.yaml
+kubectl apply -f ./templates/kubernetes-experiments-01-namespace.yaml
 
 # Build out PostgreSQL
-kubectl apply -f ./k8s/templates/postgresql/postgresql-configmap.yaml
-kubectl apply -f ./k8s/templates/postgresql/postgresql-secret.yaml
-kubectl apply -f ./k8s/templates/postgresql/postgresql-statefulset.yaml
-kubectl apply -f ./k8s/templates/postgresql/postgresql-service.yaml
+#kubectl apply -f ./k8s/templates/postgresql/postgresql-configmap.yaml
+#kubectl apply -f ./k8s/templates/postgresql/postgresql-secret.yaml
+#kubectl apply -f ./k8s/templates/postgresql/postgresql-statefulset.yaml
+#kubectl apply -f ./k8s/templates/postgresql/postgresql-service.yaml
 
 # Build out Redis
-kubectl apply -f ./k8s/templates/redis/redis-configmap.yaml
-kubectl apply -f ./k8s/templates/redis/redis-statefulset.yaml
-kubectl apply -f ./k8s/templates/redis/redis-service.yaml
+#kubectl apply -f ./k8s/templates/redis/redis-configmap.yaml
+#kubectl apply -f ./k8s/templates/redis/redis-statefulset.yaml
+#kubectl apply -f ./k8s/templates/redis/redis-service.yaml
 
 # Apply the pods and services
-kubectl apply -f ./k8s/templates/api-server/kubernetes-experiments-01-api-server-deployment.yaml
-kubectl apply -f ./k8s/templates/api-server/kubernetes-experiments-01-api-server-service.yaml
+#kubectl apply -f ./k8s/templates/api-server/kubernetes-experiments-01-api-server-deployment.yaml
+#kubectl apply -f ./k8s/templates/api-server/kubernetes-experiments-01-api-server-service.yaml
 
-kubectl apply -f ./k8s/templates/portal/kubernetes-experiments-01-portal-deployment.yaml
-kubectl apply -f ./k8s/templates/portal/kubernetes-experiments-01-portal-service.yaml
-kubectl apply -f ./k8s/templates/portal/kubernetes-experiments-01-portal-ingress.yaml
+#kubectl apply -f ./k8s/templates/portal/kubernetes-experiments-01-portal-deployment.yaml
+#kubectl apply -f ./k8s/templates/portal/kubernetes-experiments-01-portal-service.yaml
+#kubectl apply -f ./k8s/templates/portal/kubernetes-experiments-01-portal-ingress.yaml
 
-kubectl apply -f ./k8s/templates/admin-portal/kubernetes-experiments-01-admin-portal-deployment.yaml
-kubectl apply -f ./k8s/templates/admin-portal/kubernetes-experiments-01-admin-portal-service.yaml
-kubectl apply -f ./k8s/templates/admin-portal/kubernetes-experiments-01-admin-portal-ingress.yaml
+cd ./admin-portal || exit
+helm package .
+helm install kubernetes-experiments-01-admin-portal-0.0.1.tgz --namespace=kubernetes-experiments-01
+cd ..
+
+#kubectl apply -f ./k8s/templates/admin-portal/kubernetes-experiments-01-admin-portal-deployment.yaml
+#kubectl apply -f ./k8s/templates/admin-portal/kubernetes-experiments-01-admin-portal-service.yaml
+#kubectl apply -f ./k8s/templates/admin-portal/kubernetes-experiments-01-admin-portal-ingress.yaml
 
 kubectl get pods,services,deployment,statefulset --namespace=kubernetes-experiments-01
