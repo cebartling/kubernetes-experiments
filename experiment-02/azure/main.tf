bb10b5af1c7bcca2f981ea2587257f9136f18a6b
Christopher Bartling <chris.bartling@gmail.com>
2024-10-24 22:20:59 -0500

    feat: provisioning AKS cluster through terraform

--- experiment-02/azure/main.tf
+++ experiment-02/azure/main.tf
@@ -1,73 +1,38 @@
-# Configure the Azure provider
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.6.0"
-    }
-  }
-
-  required_version = ">= 1.1.0"
-}
-
 provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "aks_rg" {
-  name     = "aks-resource-group"
-  location = var.location
-}
-
-resource "azurerm_virtual_network" "aks_vnet" {
-  name                = "aks-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
-}
-
-resource "azurerm_subnet" "aks_subnet" {
-  name                 = "aks-subnet"
-  resource_group_name  = azurerm_resource_group.aks_rg.name
-  virtual_network_name = azurerm_virtual_network.aks_vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-  delegation {
-    name = "aks_delegation"
-    service_delegation {
-      name = "Microsoft.ContainerService/managedClusters"
-      actions = [
-        "Microsoft.Network/virtualNetworks/subnets/join/action"
-      ]
-    }
-  }
-}
-
-resource "azurerm_kubernetes_cluster" "aks" {
-  name                = var.aks_cluster_name
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
-  dns_prefix          = "akscluster"
-
-  default_node_pool {
-    name           = "default"
-    node_count     = 3
-    vm_size        = "Standard_DS2_v2"
-    vnet_subnet_id = azurerm_subnet.aks_subnet.id
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
-  network_profile {
-    network_plugin    = "azure"
-    network_policy    = "azure"
-    load_balancer_sku = "Standard"
-  }
+resource "azurerm_resource_group" "default" {
+  name     = "experiment-02-rg"
+  location = "West US 2"
 
   tags = {
-    environment = var.environment_tag
+    environment = var.tag_environment
   }
 }
 
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "experiment-02-aks"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix          = "experiment-02-k8s"
+  kubernetes_version  = "1.30.2"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = var.appId
+    client_secret = var.password
+  }
+
+  role_based_access_control_enabled = true
+
+  tags = {
+    environment = var.tag_environment
+  }
+}
--- experiment-02/azure/variables.tf
+++ experiment-02/azure/variables.tf
@@ -1,11 +1,19 @@
 variable "location" {
-  default = "East US"
+  default = "West US"
 }
 
 variable "aks_cluster_name" {
   default = "experiment-02-aks-cluster"
 }
 
-variable "environment_tag" {
-  default = "development"
+variable "tag_environment" {
+  default = "Demo"
+}
+
+variable "appId" {
+  description = "Azure Kubernetes Service Cluster service principal"
+}
+
+variable "password" {
+  description = "Azure Kubernetes Service Cluster password"
 }
