3224eefd9ce07cfbd87178d335e6270fe44e143c
Christopher Bartling <chris.bartling@gmail.com>
2024-10-25 08:27:53 -0500

    feat: updating k8s version

--- experiment-02/azure/main.tf
+++ experiment-02/azure/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_kubernetes_cluster" "default" {
   location            = azurerm_resource_group.default.location
   resource_group_name = azurerm_resource_group.default.name
   dns_prefix          = var.dns_prefix
-  kubernetes_version  = "1.30.2"
+  kubernetes_version  = "1.30.5"
 
   default_node_pool {
     name            = "default"
